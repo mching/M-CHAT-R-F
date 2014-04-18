748eb79a5c84cf2bfab84d3e832da35c701f7e90
Michael Ching <mikeching@yahoo.com>
2014-04-17 22:22:09 -1000

    initial commit

---
+++ pseudocode_plan.R
@@ -0,0 +1,13 @@
+# Pseudocode Analysis plan for Project
+
+# Merge pre and post data
+
+# Drop "other" empty variables
+
+# Generate descriptive stats (n, specialty vs generalist, resident vs staff, #
+# of ASD patients)
+
+# Calculate descriptive stats on perceived barriers before and after
+
+# Calculate pre and post scores on for those who completed both surveys on
+# comfort with M-CHAT-R, value of the instrument, barriers to implementation
