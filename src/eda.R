be04963cddfa1b3d3df612458ca0aa843b361af4
Michael Ching <mikeching@yahoo.com>
2014-04-18 12:19:37 -1000

    add some code to load dataset if no ProjectTemplate

--- src/eda.R
+++ src/eda.R
@@ -3,8 +3,14 @@ rm(list = ls())
 library('ProjectTemplate')
 load.project()
 
+## if no ProjectTemplate, use read.spss
+# library(foreign)
+# data <- read.spss("./data/pre.sav")
+
+
 for (dataset in project.info$data)
 {
   message(paste('Showing top 5 rows of', dataset))
   print(head(get(dataset)))
 }
+
