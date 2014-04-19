d60a04622504ce396f0b4e9ba084af9d9eea6e31
Michael Ching <mikeching@yahoo.com>
2014-04-18 15:15:11 -1000

    initial commit

---
+++ munge/01-post.R
@@ -0,0 +1,81 @@
+# Preprocessing script for post questionnaire
+# Note: Corrected data is for 28 responses only. If more are received, you need
+# to manually correct additional entries
+
+#############################
+# Keep only the variables that start with a lower case q
+#############################
+
+# Create a vector of variable names
+variable.names <- names(post) 
+
+# Create a vector of the positions where the first letter is lowercase q
+lower.q.questions <- grep("^q", variable.names) 
+rm(variable.names)
+
+# Filter dataset by these variables
+post.filtered <- post[c(2, lower.q.questions)]
+post.unfiltered <- post
+post <- post.filtered
+rm(post.filtered)
+rm(lower.q.questions)
+
+# Make list into data frame
+post <- as.data.frame(post)
+
+##########################
+# q0001_0001, 0002, 0003 : convert to variables on how an individual is involved
+# in the general pediatrics clinic
+#
+# Output variables = care_direct, care_supervise, care_no_direct_or_supervise
+#
+##########################
+post$care_direct <- as.numeric(post$q0001_0001)
+post$care_supervise <- as.numeric(post$q0001_0002)
+post$care_no_direct_or_supervise <- as.numeric(post$q0001_0003)
+
+post$care_direct[is.na(post$care_direct)] <- 0
+post$care_supervise[is.na(post$care_supervise)] <- 0
+post$care_no_direct_or_supervise[is.na(post$care_no_direct_or_supervise)] <- 0
+
+table(direct = post$care_direct, supervise = post$care_supervise, no.direct.or.supervise = post$care_no_direct_or_supervise, useNA="if") 
+
+# replace as factor variables
+post$care_direct <- as.factor(post$care_direct)
+post$care_supervise <- as.factor(post$care_supervise)
+post$care_no_direct_or_supervise <- as.factor(post$care_no_direct_or_supervise)
+
+# If no direct care or supervision, make their responses on q2-13 into NA
+a <- post$care_no_direct_or_supervise == 1
+post[a, 6:30] <- NA
+rm(a)
+
+# One person failed to respond to this question, respondent 16
+# post[40] == 0 & post[41] == 0 & post[42] == 0
+
+# make respondent 16 into NA on all roles
+post[16, 40:42] <- NA
+
+# Correct q0007. Correct answers were selected manually because of the judgment involved
+# in determining if the respondent was correct
+post$q0007_correct <- rep(0, length(post$q0007))
+post$q0007
+correct_respondents <- c(6, 9, 10, 13, 16, 21, 23)
+post$q0007_correct[correct_respondents] <- 1
+post$q0007_correct[post$q0007 == post$q0007[12]] <- NA # Make NAs for not answered in q0007
+
+# CONTINUE EDITING HERE
+pre$q0006_correct <- factor(pre$q0006_correct)
+rm(correct_respondents)
+
+# Make matching codes all uppercase to facilitate matching
+pre$q0011 <- toupper(pre$q0011)
+
+# Trim leading and trailing white space
+pre$q0011 <- gsub("^\\s+|\\s+$", "", pre$q0011)
+
+# Make "" into NAs
+pre$q0011[pre$q0011 == ""] <- NA
+
+# Rename q0011 to match_ID
+pre$match_ID <- pre$q0011
